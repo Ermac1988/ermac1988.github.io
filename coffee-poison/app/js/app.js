0104f2dcefdb8e5e3ce03cea4b8270ff67707fef
Vlad <ermac1988@mail.ru>
2018-06-05 18:44:27 +0300

    Updated coffee-poison

--- coffee-poison/app/js/app.js
+++ coffee-poison/app/js/app.js
@@ -1,2 +1,26 @@
 $(document).ready(function(){
+    $('.testimonials-box__slider').slick({
+        accessibility: true,
+        infinite: false,
+        autoplay: false,
+        autoplaySpeed: 1000,
+        slidesToShow: 3,
+        arrows: false,
+        dots: true,
+        initialSlide: 4,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
 });
