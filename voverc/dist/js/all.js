862666fc1951240ec78e4730affd351b763c60b7
Vlad <ermac1988@mail.ru>
2019-05-07 15:28:05 +0300

    Updated voverc

--- voverc/dist/js/all.js
+++ voverc/dist/js/all.js
@@ -1,2 +1,2 @@
-
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJhbGwuanMiLCJzb3VyY2VzQ29udGVudCI6W119
+$(function(){$('[data-toggle="tooltip"]').tooltip()});
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6WyIkIiwidG9vbHRpcCJdLCJtYXBwaW5ncyI6IkFBQUFBLEVBQUEsV0FDQUEsRUFBQSwyQkFBQUMiLCJmaWxlIjoiYWxsLmpzIiwic291cmNlc0NvbnRlbnQiOlsiJChmdW5jdGlvbiAoKSB7XG4gICAgJCgnW2RhdGEtdG9nZ2xlPVwidG9vbHRpcFwiXScpLnRvb2x0aXAoKVxufSkiXX0=
